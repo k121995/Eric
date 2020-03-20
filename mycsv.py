20f27a5f2ecd0249e2e13e245c5ce0a6ce168e03
Kiran Sonawane <kiran.sonawane@navsoft.in>
2020-03-20 10:11:07 +0530

    my project Eric

---
+++ mycsv.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+data = pd.read_csv (r'/home/user/Desktop/ICA_Aid_Opt_CSV.csv')   
+df = pd.DataFrame(data, columns= ['Financial Aid System Applicant ID','Total Tuition'])
+print(df)
